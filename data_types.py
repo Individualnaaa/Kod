42c7fb4e5b0077766a8478acfc88ddafa677579b
Danuta Duda <danutaduda1@gmail.com>
2026-09-10 20:23:07 +0200

    Nowy

---
+++ data_types.py
@@ -0,0 +1,18 @@
+6.023e+23
+print(0,5)
+print(1/3)
+print(1.67e-10)
+print(6.023e+23)
+
+digit = 102321.23092183091209382109380912
+digit = digit.__round__(3)
+print(digit)
+
+x = True
+y = False
+z = None
+print(type(x))
+
+warunek_spelniony = (10 > 5) # True
+warunek_niespelniony = (10 < 5) # False 
+print(warunek_spelniony)
