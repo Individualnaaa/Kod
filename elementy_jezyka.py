8653c639eda6f6c5c18c11a1e61936dbaefde8ca
Danuta Duda <danutaduda1@gmail.com>
2026-09-12 13:19:58 +0200

    elementy języka

---
+++ elementy_jezyka.py
@@ -0,0 +1,22 @@
+
+
+
+# not negacja
+# or lub
+# iloczyn logiczny
+
+# wynik = True or False
+# print(wynik)
+
+# imie = "Kasia"
+# wiek = 18
+
+# wynik = imie == "Kasia" and wiek >= 18
+
+
+# łączenie
+
+
+
+
+print(wynik)
