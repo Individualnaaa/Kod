bca681fee2eca05672da8fa418cb66a2500bd3cc
Danuta Duda <danutaduda1@gmail.com>
2026-09-12 13:46:46 +0200

    if

---
+++ instrukcje_warunkowe.py
@@ -0,0 +1,24 @@
+# imie = "Kasia"
+# wiek = 50
+# wynik = imie == "Kasia" and wiek >= 18
+
+# if imie == "Kasia":
+#     print("Kod się wykonuje bo True")
+#     print("To jest wewnątrz bloku if")
+# else:
+#     print("Bleeee false to drukuję to")
+
+# print("To jest poza blokiem, wykona się zawsze")
+
+imie = "Zosia"
+wiek = 19
+
+#Sprawdz czy osoba jest kobieta pełnoletnią jesli tak to "energetyki rządzą"
+# jesli nie to "Wole jabłka"
+
+print (imie[-1])
+
+if imie.endswith("a") and wiek >= 18:
+    print("Energetyki rządzą")
+else:
+    print("Wolę jabłka")
